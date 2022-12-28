1933cd9ab2a5d82cd15f4d498b22ac617fcf1670
William Gao <williamgao520@gmail.com>
2022-12-28 12:17:56 -0800

    update GetVersion()

--- internal/utils.go
+++ internal/utils.go
@@ -14,11 +14,10 @@ func GetVersion(filename string) (string, error) {
 	}
 	defer f.Close()
 
-	sc := bufio.NewScanner(f)
 	var line string
+	sc := bufio.NewScanner(f)
 	for sc.Scan() {
 		line = strings.TrimSpace(sc.Text())
-
 		// find the first non-empty, non-comment line in the WDL file
 		if len(line) > 0 && !strings.HasPrefix(line, "#") {
 			break
@@ -29,10 +28,10 @@ func GetVersion(filename string) (string, error) {
 		return "", fmt.Errorf("%w", err)
 	}
 
-	// no version string... infer that it is draft-2
-	if !strings.HasPrefix(line, "version") {
-		return "draft-2", nil
+	if strings.HasPrefix(line, "version ") {
+		return strings.Split(line, " ")[1], nil
 	}
 
-	return strings.Split(line, "version ")[1], nil
+	// no version string... infer that it is draft-2
+	return "draft-2", nil
 }
